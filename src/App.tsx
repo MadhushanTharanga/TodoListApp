88fc633f8a1b2524050b501eacfb0ff33ef1e02b
MadhushanTharanga <madhushan0617@gmail.com>
2025-04-10 00:17:24 +0530

    updated app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-function App() {
-    return (
-      <div className="bg-gray-100 p-4">
-        <p className="text-3xl font-bold text-center text-blue-600">Hello, Tailwind CSS!</p>
+import React from "react";
+import './index.css';
+
+const App: React.FC=()=>{
+  return(
+    <div className="h-screen bg-white flex flex-col ">
+      <div className="h-16 bg-white flex">
+        <div className="w-64 bg-red-100 flex justify-center items-center">
+          <h1 className="text-md font-semibold  ">Madhushan tharanga</h1>
+        </div>
+        <div className="w-200 bg-red-200"></div>
+        <div className="flex-1 bg-red-300"></div>
       </div>
-    );
-  }
-  
-  export default App;
+      <div className="flex-1 bg-red-200 flex">
+        <div className="w-64 bg-gray-200"></div>
+        <div className="w-200 bg-blue-200"></div>
+        <div className="flex-1 bg-green-100"></div>
+      </div>
+    </div>
+  )
+}
+export default App;
